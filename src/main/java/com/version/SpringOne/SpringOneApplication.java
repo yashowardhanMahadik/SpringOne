1189ecd4d79a0b1023351b2a8a927a3ed20eceba
yashowardhanMahadik <yashowardhanmahadik@gmail.com>
2021-09-29 00:42:04 +0530

    minor change

--- src/main/java/com/version/SpringOne/SpringOneApplication.java
+++ src/main/java/com/version/SpringOne/SpringOneApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
